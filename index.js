9457b322213b3c597c432720259deedf72256fd7
Akunna Onyekachi <93290304+akunna1@users.noreply.github.com>
2024-02-10 16:50:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+// ROUTING
+const express = require("express"); // Importing the Express framework and assigning it to the 'express' variable
+const app = express(); // Creating an instance of the Express application
+
+const port = 8083; // the server would listen to this port
+const path = require("path"); // Importing the 'path' module for working with file paths
+
+// DATABASE INTERACTIONS
+// Using google cloud to store resume and cover letter
+const { Storage } = require("@google-cloud/storage"); // Importing the Google Cloud Storage library and assigning it to 'Storage'
+const multer = require("multer"); // Importing the 'multer' library for handling file uploads
+const bodyParser = require("body-parser"); // Importing the 'body-parser' middleware for parsing HTTP request bodies
+
+// STATIC FILE SERVING
+// Defining the source directory for static files i.e term_project.html, term_project.js and term_project.css
+const src = path.join(__dirname, "front_end");
+app.use(express.static(src)); // Serving static files from the 'front_end' directory
+
+// FILE UPLOAD CONFIGURATION
+// Setting up 'Multer' to handle file uploads, using in-memory storage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+// GOOGLE CLOUD STORAGE CONFIGURATION
+// Configuring Google Cloud Storage using project ID and key file
+let projectId = "akunna-project-1";
+let keyFilename = "mykey.json";
+const storageClient = new Storage({
+  projectId,
+  keyFilename,
+});
+const bucket = storageClient.bucket("neptune-tech-proj"); // Specifying the name of the Google Cloud Storage bucket
+
+// FILE UPLOAD ROUTE
+// Defining a route to handle file uploads when a POST request is made to '/submit_application'
+app.post("/submit_application", upload.fields([{ name: "resume" }, { name: "cover_letter" }]), (req, res) => {
+  // req.files contains the uploaded files
+
+  // Extracting the uploaded files (resume and cover_letter) and the other form data
+  const resumeFile = req.files["resume"][0];
+  const coverLetterFile = req.files["cover_letter"][0];
+  const {
+    firstName, middleName, lastName, dob, gender, phoneNumber, email, homeNumber, UnitNumber, streetName,
+    city, usState, zipCode, degreeType, major, uni_name, gradDate, current_enrollment_Details, degreeType_1, major_1,
+    current_employer, current_job_title, current_job_duration, previous_employers, linkInput, linkInput_1,
+    skills, certs, refs
+  } = req.body;
+
+  // Creating a new data object for the job application
+  const applicationData = {
+    firstName, middleName, lastName, dob, gender, phoneNumber, email, homeNumber, UnitNumber, streetName,
+    city, usState, zipCode, degreeType, major, uni_name, gradDate, current_enrollment_Details, degreeType_1, major_1,
+    current_employer, current_job_title, current_job_duration, previous_employers, linkInput, linkInput_1,
+    skills, certs, refs
+  };
+
+  // Unique ID generator
+  function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+
+        ( 
+            c^
+            (crypto .getRandomValues (new Uint8Array (1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+    );
+  }
+
+  // GOOGLE CLOUD STORAGE FILE UPLOAD
+  // Uploading form data as a .txt file to Google Cloud Storage with a unique ID
+  const uniqueId = uuidv4();
+  const formDataFileName = `form_data_${uniqueId}.txt`;
+  const formDataFile = bucket.file(formDataFileName);
+
+  formDataFile.save(JSON.stringify(applicationData, null, 2), {
+    metadata: {
+      contentType: "text/plain" // Set content type to plain text
+    }
+  }, (err) => {
+    if (err) {
+      console.error("Error saving form data:", err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      // Creating objects for uploading files to Google Cloud Storage
+      const resumeFileUpload = bucket.file(resumeFile.originalname);
+      const coverLetterFileUpload = bucket.file(coverLetterFile.originalname);
+
+      // Creating write streams for the files and saving the uploaded data to Google Cloud Storage
+      const resumeFileStream = resumeFileUpload.createWriteStream();
+      const coverLetterFileStream = coverLetterFileUpload.createWriteStream();
+
+      // Ending the write streams, effectively uploading the files
+      resumeFileStream.end(resumeFile.buffer);
+      coverLetterFileStream.end(coverLetterFile.buffer);
+
+      // Sending a successful response back to the client
+      res.status(200).send("Thank you for taking the first step in joining Neptune Technologies. We've received your application and will carefully review your qualifications. Stay tuned for updates on your application status.");
+    }
+  });
+});
+
+// BODY PARSER MIDDLEWARE
+// Using 'body-parser' middleware to parse URL-encoded and JSON request bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ROOT ROUTE
+// Defining a route for the root URL that serves 'term_project.html'
+app.get("/", (req, res) => {
+  res.sendFile(path.join(src, "term_project.html"));
+});
+
+// SERVER START
+// Starting the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
---
+++ term_project.js
@@ -0,0 +1,140 @@
+//Tab Switching
+// Function to show the specified tab and hide others
+function showTab(tabId) {
+    // Hiding all tabs
+    var tabs = document.querySelectorAll('.tab-content > div');
+    tabs.forEach(function(tab) {
+        tab.style.display = 'none';
+    });
+
+    // Showing the selected tab
+    var selectedTab = document.getElementById(tabId);
+    selectedTab.style.display = 'block';
+
+    // Removing 'active' class from all nav links
+    var navLinks = document.querySelectorAll('.nav-tabs .nav-link');
+    navLinks.forEach(function(link) {
+        link.classList.remove('active');
+    });
+
+    // Adding 'active' class to the clicked nav link
+    var clickedLink = document.getElementById(tabId + '-tab');
+    clickedLink.classList.add('active');
+
+    // Storing the active tab ID in localStorage
+    localStorage.setItem('activeTab', tabId);
+}
+
+// Set initial active tab to About Us and change its background color
+window.onload = function() {
+    // Get the ID of the active tab from localStorage
+    var activeTab = localStorage.getItem('activeTab');
+
+    // If there's an active tab stored, show it
+    if (activeTab) {
+        showTab(activeTab);
+    } else { // If not, default to About Us
+        showTab('about-us');
+    }
+};
+
+
+// Current Enrollment Dropdown Menu
+function show_current_enrollment_Details() {
+    var current_enrollment_Dropdown = document.getElementById("current_enrollment_Dropdown");
+    var current_enrollment_Details = document.getElementById("current_enrollment_Details");
+
+    if (current_enrollment_Dropdown.value === "Yes") {
+        current_enrollment_Details.style.display = "block";
+    } else {
+        current_enrollment_Details.style.display = "none";
+    }
+}
+
+// Validating Link
+function validateLink(inputId) {
+    var linkInput = document.getElementById(inputId); // Getting the input element
+    var inputValue = linkInput.value.trim(); // Getting the entered value
+
+    var urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w.-]*)*\/?$/; // Regular expression to match a URL pattern
+
+    if (urlPattern.test(inputValue)) {   // Testing if the entered value matches the URL pattern
+        alert("Valid link format!");
+    } else {
+        alert("Invalid link format. Please enter a valid URL.");
+    }
+}
+
+// Today's Date
+const today = new Date();
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, '0');
+const day = String(today.getDate()).padStart(2, '0');
+const formattedDate = `${year}/${month}/${day}`;
+document.getElementById('date').value = formattedDate;
+
+// Enabling the Submit Button
+function checkFormValidity() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const dob = document.getElementById('dob').value;
+    const gender = document.getElementById('gender').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const email = document.getElementById('email').value;
+    const homeNumber = document.getElementById('homeNumber').value;
+    const streetName = document.getElementById('streetName').value;
+    const city = document.getElementById('city').value;
+    const usState = document.getElementById('usState').value;
+    const zipCode = document.getElementById('zipCode').value;
+    const degreeType = document.getElementById('degreeType').value;
+    const major = document.getElementById('major').value;
+    const uni_name = document.getElementById('uni_name').value;
+    const gradDate = document.getElementById('gradDate').value;
+    const current_employer = document.getElementById('current_employer').value;
+    const current_job_title = document.getElementById('current_job_title').value;
+    const current_job_duration = document.getElementById('current_job_duration').value;
+    const skills = document.getElementById('skills').value;
+    const certs = document.getElementById('certs').value;
+    const resume = document.getElementById('resume').value;
+    const cover_letter = document.getElementById('cover_letter').value;
+    const affirmation1 = document.getElementById('affirmation1').value;
+    const fullname = document.getElementById('fullname').value;
+    const submitBtn = document.getElementById('submitBtn');
+    if (firstName && lastName && dob && gender && phoneNumber && email && homeNumber && streetName && city
+        && usState && zipCode && degreeType && major && uni_name && gradDate && current_employer && current_job_title 
+        && current_job_duration && skills && certs && resume && cover_letter && affirmation1 && fullname) {
+        submitBtn.disabled = false;
+    } else {
+        submitBtn.disabled = true;
+    }
+}
+
+// Event listeners for form inputs
+document.getElementById('firstName').addEventListener('input', checkFormValidity);
+document.getElementById('lastName').addEventListener('input', checkFormValidity);
+document.getElementById('dob').addEventListener('input', checkFormValidity);
+document.getElementById('gender').addEventListener('input', checkFormValidity);
+document.getElementById('phoneNumber').addEventListener('input', checkFormValidity);
+document.getElementById('email').addEventListener('input', checkFormValidity);
+document.getElementById('homeNumber').addEventListener('input', checkFormValidity);
+document.getElementById('streetName').addEventListener('input', checkFormValidity);
+document.getElementById('city').addEventListener('input', checkFormValidity);
+document.getElementById('usState').addEventListener('input', checkFormValidity);
+document.getElementById('zipCode').addEventListener('input', checkFormValidity);
+document.getElementById('degreeType').addEventListener('input', checkFormValidity);
+document.getElementById('major').addEventListener('input', checkFormValidity);
+document.getElementById('uni_name').addEventListener('input', checkFormValidity);
+document.getElementById('gradDate').addEventListener('input', checkFormValidity);
+document.getElementById('current_employer').addEventListener('input', checkFormValidity);
+document.getElementById('current_job_title').addEventListener('input', checkFormValidity);
+document.getElementById('current_job_duration').addEventListener('input', checkFormValidity);
+document.getElementById('skills').addEventListener('input', checkFormValidity);
+document.getElementById('certs').addEventListener('input', checkFormValidity);
+document.getElementById('resume').addEventListener('input', checkFormValidity);
+document.getElementById('cover_letter').addEventListener('input', checkFormValidity);
+document.getElementById('affirmation1').addEventListener('input', checkFormValidity);
+document.getElementById('fullname').addEventListener('input', checkFormValidity);
+
+// Initial form validation check
+checkFormValidity();
+
